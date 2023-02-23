4d3670a7cef427cc55cabcab6636591f2a0767c1
David Oliveira <53923266+david-oliveira17@users.noreply.github.com>
2023-02-23 14:10:27 +0000

    (23/02)

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/****************** ROTAS ******************/
+const users = require('./routes/users')
+app.use('/users', users)
+
 
 module.exports = app;
