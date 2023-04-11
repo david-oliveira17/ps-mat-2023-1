50a1b9e1ec6536e08d599e9f10397e7b747936da
David Oliveira <53923266+david-oliveira17@users.noreply.github.com>
2023-04-11 14:03:27 +0000

    (11/04)Criação do projeto front-end

--- back-end/lib/auth.js
+++ back-end/lib/auth.js
@@ -2,6 +2,17 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
 
+    const bypassRoutes = [
+        {url: '/users/login', method: 'POST'}
+    ]
+
+    for(let route of bypassRoutes) {
+        if(route.url === req.url && route.method === req.method) {
+            next()
+            return
+        }
+    }
+
     //É necessário ter o token para continuar
     const bearerHeader = req.headers['authorization']
     //O token não foi passado -> 403
