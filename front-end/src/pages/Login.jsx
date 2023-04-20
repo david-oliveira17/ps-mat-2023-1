c8728035fde3c0f489545efc7079d0fc62afec3f
David Oliveira <53923266+david-oliveira17@users.noreply.github.com>
2023-04-20 14:14:10 +0000

    Criação formulário de login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,10 +1,74 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
 export default function Login() {
+
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    function handleChange(event) {
+        if (event.target.name === 'email') setEmail (event.target.value)  
+        else setPassword(event.target.value)
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault()      // Impede o recarregamento da página
+        try {
+          let response = await fetch('http://localhost:3000/users/login', {
+            method: "POST",
+            body: JSON.stringify({email, password}),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+          })
+          console.log({response})
+        }
+        catch(error) {
+          console.error(error)
+        }
+      }
+
     return (
         <>
-        <h1>Página de login</h1>
-        <p>Futura página de login</p>
+            <Typography variant="h3" component="h1" sx={{ textAlign: 'center' }}>
+                Autentique-se
+            </Typography>
+            <Paper sx={{
+                width: '512px',
+                maxWidth: '90%',
+                margin: '25px auto 0 auto',
+                p: '12px'
+            }}>
+                <Typography variant="h5" component="div">
+                    <form onSubmit={handleSubmit}>
+                        <TextField 
+                            id="email"
+                            className='form-field'
+                            name="email"
+                            label="E-mail" 
+                            variant="filled"
+                            fullWidth
+                            onChange={handleChange}
+                            value={email}
+                        />
+                        <TextField
+                            id="password"
+                            className='form-field'
+                            name="password"
+                            label="Senha" 
+                            variant="filled"
+                            type="password"
+                            fullWidth
+                            onChange={handleChange}
+                            value={password}
+                        />
+                        <Button variant="contained" type="submit" color="secondary" fullWidth>
+                            Enviar
+                        </Button>
+                    </form>
+                </Typography>
+            </Paper>
         </>
     )
 }
